b84e79e40c44433a19634f0d99c50075179b9413
jiangzg <2992265870@qq.com>
2017-12-26 17:16:19 +0800

    编辑器图片上传优化

--- src/index.js
+++ src/index.js
@@ -1,18 +1,22 @@
+import 'core-js/es6/map'
+import 'core-js/es6/set'
+import 'raf/polyfill'
 
 import './style/less/base.less';
 import React from 'react';
 import ReactDom from 'react-dom';
-import { Provider } from 'react-redux';
+import {Provider} from 'react-redux';
 
 import App from './app';
 import configureStore from './redux/store/configureStore';
+
 let store = configureStore();
 
 ReactDom.render(
-  <Provider store={store}>
-	<App />
-  </Provider>,
-  document.getElementById('app')
+	<Provider store={store}>
+		<App/>
+	</Provider>,
+	document.getElementById('app')
 );
 
 
